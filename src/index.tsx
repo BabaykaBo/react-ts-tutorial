d2049cfa09ff29533bb18acc86bb9eb42f67bf43
zaaqq135@gmail.com <Oleh>
2024-12-02 21:24:25 +0200

    added Pizza component

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,10 @@
 import React from "react";
 // import ReactDOM from "react-dom";
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
+import App from "./components/App";
 
-const App = () => {
-  return <div>Hello World</div>;
-};
-
-const container = document.getElementById('root');
-
-if (container) {
-  const root = createRoot(container); // createRoot(container!) if you use TypeScript
-  root.render(<App />);
-}
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(<App />);
 
 // ReactDOM.render(<App />, document.getElementById("root"));
