8b85341d0dedb8bf729945a337f2a6c00e74590e
Oleh <zaaqq135@gmail.com>
2024-12-11 22:50:40 +0200

    added Special Offer Pizza

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,10 +5,12 @@ import Cart from "./Cart";
 import AppCSS from "./App.module.css";
 import PizzaSVG from "../svg/pizza.svg";
 import AppStateProvider from "./AppState";
+import SpecialOffer from "./SpecialOffer";
 
 console.log(AppCSS);
 
 const App = () => {
+  const specialOfferPizza = pizzas.find((pizza) => pizza.specialOffer);
   return (
     <AppStateProvider>
       <div className={AppCSS.container}>
@@ -17,7 +19,8 @@ const App = () => {
           <div className={AppCSS.siteTitle}>Delicious Pizza</div>
           <Cart />
         </div>
-        <ul>
+        {specialOfferPizza && <SpecialOffer pizza={specialOfferPizza} />}
+        <ul className={AppCSS.pizzaList}>
           {pizzas.map((pizza) => {
             return <Pizza key={pizza.id} pizza={pizza} />;
           })}
